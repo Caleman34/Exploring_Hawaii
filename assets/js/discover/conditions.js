6f46bd57899509946f10a4b5cdd6654924cca060
Chris Aleman <chris.aleman34@gmail.com>
2021-10-02 11:59:16 -0500

    layers added

--- assets/js/discover/conditions.js
+++ assets/js/discover/conditions.js
@@ -1,6 +1,8 @@
 // hard code for backup due to CORS error occuring often
 var beachJson = "/assets/js/discover/conditions.json";
 var volcanoJson = "/assets/js/discover/volcanoData.json";
+var coraljson = "/assets/js/discover/corals.json";
+var parkjson = "/assets/js/discover/Parks.geojson";
 
 var map = L.map("map", {
   center: [20.438043, -157.462667],
@@ -49,12 +51,17 @@ var volcanoIcon = L.icon({
   iconSize: [20, 25]
 });
 
+var coralIcon = L.icon({
+  iconUrl: '/assets/img/coral_icon.png',
+  iconSize: [20, 25]
+});
+
 // Beach Markers layer group
-var beachMarker = L.layerGroup();
+var beachMarker = L.layerGroup().addTo(map);
 
 d3.json(beachJson, function (beachData) {
   for (var i = 0; i < beachData.length; i++) {
-    var beachMarkers = L.marker([beachData[i].lat, beachData[i].lon], {icon: umbrellaIcon}).bindPopup("<h4>" + beachData[i].beach + "</h4>" + "<hr>" +
+    var beachMarkers = L.marker([beachData[i].lat, beachData[i].lon], { icon: umbrellaIcon }).bindPopup("<h4>" + beachData[i].beach + "</h4>" + "<hr>" +
       "<h6>Island: " + beachData[i].island + "</h6>" +
       "<h6>Shore: " + beachData[i].shore + "</h6>" +
       "<hr>" + "<h6>Temperature: " + beachData[i].temp + " F" + "</h6>" +
@@ -70,14 +77,91 @@ var volcanoMarker = L.layerGroup();
 
 d3.json(volcanoJson, function (volcanoData) {
   for (var i = 0; i < volcanoData.length; i++) {
-    var volcanoMarkers = L.marker([volcanoData[i].lat, volcanoData[i].lon], {icon: volcanoIcon}).bindPopup("<h4>Volcano Name: " + volcanoData[i].volcano_name + "</h4>" + "<hr>" +
-    "<h6>Type: " + volcanoData[i].type + "</h6>" +
-    "<h6>Status: " + volcanoData[i].status + "</h6>" +
-    "<hr>" + "<h6>Last Known Eruption: " + volcanoData[i].last_known_eruption_type + " F" + "</h6>" +
-    "<h6>Elevation: " + volcanoData[i].elevation_m + " meters</h6>");
+    var lat = volcanoData[i].lat;
+    var lon = volcanoData[i].lon;
+    var volcanoMarkers = L.marker([lat,lon], { icon: volcanoIcon }).bindPopup("<h4>Volcano Name: " + volcanoData[i].volcano_name + "</h4>" + "<hr>" +
+      "<h6>Type: " + volcanoData[i].type + "</h6>" +
+      "<h6>Status: " + volcanoData[i].status + "</h6>" +
+      "<hr>" + "<h6>Last Known Eruption: " + volcanoData[i].last_known_eruption_type + " F" + "</h6>" +
+      "<h6>Elevation: " + volcanoData[i].elevation_m + " meters</h6>").on({click: function(e) {
+        map.panTo([e.lat, e.lon]).zoomIn(7);
+      }
+      });
     volcanoMarker.addLayer(volcanoMarkers);
   }
 });
+
+
+// // coral marker layer group
+var coralMarker = L.layerGroup();
+
+d3.json(coraljson, function (coralData) {
+  for (var i = 0; i < coralData.length; i++) {
+    var coralMarkers = L.marker([coralData[i].latitude, coralData[i].longitude], {icon: coralIcon }).bindPopup("<h4>Name: " + coralData[i].VernacularNameCategory + "</h4>" + "<hr>" +
+    "<h6>Locality: " + coralData[i].Locality + "</h6>" +
+    "<h6>Scientific Name: " + coralData[i].ScientificName + "</h6>" +
+    "<h6>Depth: " + coralData[i].DepthInMeters + " meters</h6>")
+    coralMarker.addLayer(coralMarkers);
+  }
+});
+
+// park layer group
+
+var parks = L.layerGroup().addTo(map);
+
+d3.json(parkjson, function (parkData) {
+  createFeatures(parkData.features);
+});
+
+function createFeatures(parkjson) {
+  function onEachFeature(feature, parklayer) {
+
+    parklayer.on({
+      // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+      mouseover: function(event) {
+        parklayer = event.target;
+        parklayer.setStyle({
+          fillOpacity: 0.9
+        });
+      },
+      // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+      mouseout: function(event) {
+        parklayer = event.target;
+        parklayer.setStyle({
+          fillOpacity: 0.5
+        });
+      },
+      // When a feature (park) is clicked, it is enlarged to fit the screen
+      click: function(event) {
+        map.fitBounds(event.target.getBounds());
+      }
+    });
+
+    parklayer.bindPopup("<h4>" + feature.properties.name +"</h4>" + "<hr>" + "<h6>Type of Park: "
+      + feature.properties.type + "</h6>");
+  }
+
+  function style(feature, layer) {
+    return {
+      opacity: 0.5,
+      radius: 5,
+      weight: 1,
+      color: "green",
+      fillColor: "green",
+      fillOpacity: 0.5
+    }
+  }
+
+  var park = L.geoJSON(parkjson, {
+    pointToLayer: function(_geometry, coordinates) {
+      return L.circleMarker(coordinates);
+  },
+  onEachFeature: onEachFeature,
+  style: style
+});
+parks.addLayer(park);
+  };
+
 
 // Create a baseMaps object to hold the lightmap layer
 var baseMaps = {
@@ -89,9 +173,11 @@ var baseMaps = {
 // Create an overlayMaps object to hold the beach conditions layer
 var overlayMaps = {
   "<img src='/assets/img/beachUmbrella_icon.png' height=20> Beaches": beachMarker,
-    "<img src='/assets/img/volcano_icon.png' height=20> Volcanoes": volcanoMarker
+  "<img src='/assets/img/park_icon.png' height=20> Parks": parks,
+  "<img src='/assets/img/volcano_icon.png' height=20> Volcanoes": volcanoMarker,
+  "<img src='/assets/img/coral_icon.png' height=20> Coral Sites": coralMarker
 };
 
 L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
+  collapsed: true
 }).addTo(map);
